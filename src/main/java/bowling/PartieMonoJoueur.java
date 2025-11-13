75c25a49fa71aa66356efbeb8946953034aeba94
Motis <vpnloicvpn@gmail.com>
2025-11-13 11:39:42 +0100

    debut (a continuer)

--- src/main/java/bowling/PartieMonoJoueur.java
+++ src/main/java/bowling/PartieMonoJoueur.java
@@ -10,6 +10,11 @@ public class PartieMonoJoueur {
 	/**
 	 * Constructeur
 	 */
+
+	private boolean lastWasStrike = false;
+	private boolean lastWasSpare = false;
+
+	
 	public PartieMonoJoueur() {
 	}
 
@@ -21,7 +26,38 @@ public class PartieMonoJoueur {
 	 * @return vrai si le joueur doit lancer à nouveau pour continuer son tour, faux sinon	
 	 */
 	public boolean enregistreLancer(int nombreDeQuillesAbattues) {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		if (estTerminee()) {
+			throw new IllegalStateException("La partie est terminée");
+		}
+		if (numeroProchainLancer() == 1) {
+			// Premier lancer du tour
+			if (nombreDeQuillesAbattues == 10) {
+				// Strike
+				// Passer au tour suivant
+				score(nombreDeQuillesAbattues);
+				return false;
+			} else {
+				// Pas strike, le joueur peut lancer une deuxième fois
+				return true;
+			}
+		} else if (numeroProchainLancer() == 2 && nombreDeQuillesAbattues == 10) {
+			// Deuxième lancer du tour et spare
+			// Passer au tour suivant
+			score(10);
+			return false;
+		} else if (numeroProchainLancer() == 2) {
+			// Deuxième lancer du tour
+			// Passer au tour suivant
+			score(nombreDeQuillesAbattues);
+			return false;
+		} else if (numeroTourCourant() == 10 && numeroProchainLancer() == 3) {
+			// Troisième lancer (seulement au 10ème tour)
+			// Passer au tour suivant
+			score(nombreDeQuillesAbattues);
+			return false;
+		}
+		return true;
+		//throw new UnsupportedOperationException("Pas encore implémenté");
 	}
 
 	/**
@@ -30,8 +66,12 @@ public class PartieMonoJoueur {
 	 * abattent 0 quille.
 	 * @return Le score du joueur
 	 */
-	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+	public int score(int nombreDeQuillesAbattues) {
+		if (!estTerminee()) {
+			return 0;
+		}
+		return nombreDeQuillesAbattues;
+		//throw new UnsupportedOperationException("Pas encore implémenté");
 	}
 
 	/**
